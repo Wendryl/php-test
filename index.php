916ba7b2f73bcebf33b6aa4e9d5b7be8d3c2d7b0
Wendryl <wendryl10000@gmail.com>
2020-10-28 22:05:31 -0300

    Add index page header

--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Teste PHP | OnCar
+        </title>
+        <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="views/assets/styles.css"/>
+        <link rel="stylesheet" href="vendor/fortawesome/font-awesome/css/all.css"/>
+    </head>
+    <body>
+        <div class="container bg-light border border-2 p-3">
+            <h4 class="text-primary">TESTE</h4>
+        </div>
+        <div class="container bg-light p-3 border border-secondary border-2">
+            <div class="input-group">
+                <input type="text" class="form-control border border-secondary border-2" placeholder="Buscar veículo" aria-label="Buscar veículo" aria-describedby="button-addon4">
+                <div class="input-group-append" id="button-addon4">
+                    <button class="btn btn-outline-secondary border border-secondary border-2 bg-secondary" type="button">
+                        <i class="fas fa-search text-secondary bg-secondary"></i>
+                    </button>
+                    <button class="btn btn-outline-secondary border border-secondary border-2 bg-primary" type="button">
+                        <i class="fas fa-plus text-light"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
