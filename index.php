c0ebad06e73707613040ed82942d6e4ca72ceabc
Wendryl <wendryl10000@gmail.com>
2020-10-29 21:53:23 -0300

    Add vehicle list

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         </div>
         <div class="container bg-light p-3 border border-secondary border-2">
             <div class="input-group">
-                <input type="text" class="form-control border border-secondary border-2" placeholder="Buscar veículo" aria-label="Buscar veículo" aria-describedby="button-addon4">
+                <input type="text" class="form-control border border-secondary border-2 rounded-lg" placeholder="Buscar veículo" aria-label="Buscar veículo" aria-describedby="button-addon4">
                 <div class="input-group-append" id="button-addon4">
                     <button class="btn btn-outline-secondary border border-secondary border-2 bg-secondary" type="button">
                         <i class="fas fa-search text-secondary bg-secondary"></i>
@@ -26,6 +26,93 @@
                 </div>
             </div>
         </div>
-    </div>
-</body>
+        <div class="container border border-2 mt-3">
+            <div class="row">
+                <div class="col-8 p-3">
+                    <div class="bg-light pt-4 px-3 pb-2 shadow-sm">
+                        <h5 class="text-secondary">Lista de veículos</h5>
+                        <ul class="list-group striped-list">
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                                <div class="d-inline-block">
+                                    <h6>FIAT</h6>
+                                    <strong class="text-primary">Uno Vivace</strong>
+                                    <p class="text-secondary"><b>2015</b></p>
+                                </div>
+                                <span class="d-flex">
+                                    <a class="btn text-primary">
+                                        <i class="fas fa-edit fa-2x"></i>
+                                    </a>
+                                </span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                                <div class="d-inline-block">
+                                    <h6>FIAT</h6>
+                                    <strong class="text-primary">Uno Vivace</strong>
+                                    <p class="text-secondary"><b>2015</b></p>
+                                </div>
+                                <span class="d-flex">
+                                    <a class="btn text-primary">
+                                        <i class="fas fa-edit fa-2x"></i>
+                                    </a>
+                                </span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                                <div class="d-inline-block">
+                                    <h6>FIAT</h6>
+                                    <strong class="text-primary">Uno Vivace</strong>
+                                    <p class="text-secondary"><b>2015</b></p>
+                                </div>
+                                <span class="d-flex">
+                                    <a class="btn text-primary">
+                                        <i class="fas fa-edit fa-2x"></i>
+                                    </a>
+                                </span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center p-3">
+                                <div class="d-inline-block">
+                                    <h6>FIAT</h6>
+                                    <strong class="text-primary">Uno Vivace</strong>
+                                    <p class="text-secondary"><b>2015</b></p>
+                                </div>
+                                <span class="d-flex">
+                                    <a class="btn text-primary">
+                                        <i class="fas fa-edit fa-2x"></i>
+                                    </a>
+                                </span>
+                            </li>
+                        </ul>
+                        <nav aria-label="...">
+                            <ul class="pagination pagination-sm justify-content-end mt-5">
+                                <li class="page-item disabled">
+                                    <a class="page-link px-3" href="#">
+                                        <i class="fas fa-angle-double-left"></i>
+                                    </a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link px-3" href="#">
+                                        <i class="fas fa-chevron-left"></i>
+                                    </a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link px-3" href="#">
+                                        <i class="fas fa-chevron-right"></i>
+                                    </a>
+                                </li>
+                                <li class="page-item">
+                                    <a class="page-link px-3" href="#">
+                                        <i class="fas fa-angle-double-right"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-4 p-3">
+                    <div class="bg-light w-100">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
